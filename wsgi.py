9c1ca24c2cf35f6a627a70390ea58aba0c815145
柳熠东 <liuyidong@hiynn.com>
2017-08-17 11:56:22 -0400

    test

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,22 @@
-from app import app as application
-
+from flask import Flask, flash, redirect, render_template, request, session, abort
+application = Flask(__name__)
+ 
+@application.route("/")
+def index():
+    return "Index!"
+ 
+@application.route("/hello")
+def hello():
+    return "Hello World!"
+ 
+@application.route("/members")
+def members():
+    return "Members"
+ 
+@application.route("/members/<string:name>/")
+def getMember(name):
+    return render_template(
+        'test.html',name=name)
+ 
 if __name__ == "__main__":
     application.run()
